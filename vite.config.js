4b30ecadc66e26176c02f4d8eb597e21436254b6
Jorge Castillo <jmcastillo9915@gmail.com>
2025-05-13 15:23:21 -0500

    [test] deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(() => {
   const config = {
     plugins: [react()],
-    base: "/",
+    base: "/pruebaDespliegue/",
   };
   return config;
 });
