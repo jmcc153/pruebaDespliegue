8665de3d9faa33088c26eb07ae7e859e26e233d1
Jorge Castillo <jmcastillo9915@gmail.com>
2025-05-21 23:26:28 -0500

    [Fix] yml envs

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig(() => {
-  const config = {
-    plugins: [react()],
-    base: "/pruebaDespliegue/",
-  };
-  return config;
-});
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: env.VITE_BASE_URL || '/',
+  };
+});
